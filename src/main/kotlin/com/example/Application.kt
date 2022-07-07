d03fedcca69e706f080b32db407b646caaec624d
Krystian Gaj <krystian0120@outlook.com>
2022-07-07 03:18:31 +0200

    Remove redundant logging

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,11 +4,9 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
 import com.example.plugins.configureOauth
-import io.ktor.server.application.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        log.debug(System.getenv("DATABASE_URL"))
         configureSessions()
         configureCors()
         configureOauth()
